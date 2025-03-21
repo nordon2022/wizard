8245be5a6d70f10e04277f95ce016cd016cd1c04
Владимир Nordon <nordon173@gmail.com>
2025-03-22 01:37:05 +0300

    env

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '..//auth/users/user.entity';
 import { AuthModule } from '../auth/auth.module';
@@ -7,16 +8,21 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // Настройка ConfigModule
+      isGlobal: true, // Делает конфигурацию доступной во всем приложении
+      envFilePath: '.env', // Указывает путь к файлу .env
+    }),
     AuthModule,
     UserModule,
     // Конфигурация подключения к базе данных PostgreSQL
     TypeOrmModule.forRoot({
       type: 'postgres', // Тип базы данных (PostgreSQL)
-      host: 'localhost', // Адрес базы данных (обычно localhost)
-      port: 5432, // Порт PostgreSQL
-      username: 'postgres', // Имя пользователя базы данных
-      password: 'postgres', // Пароль базы данных
-      database: 'postgres', // Имя базы данных
+      host: process.env.DB_HOST, // Адрес базы данных (обычно localhost)
+      port: +process.env.DB_PORT, // Порт PostgreSQL
+      username: process.env.DB_USERNAME, // Имя пользователя базы данных
+      password: process.env.DB_PASSWORD, // Пароль базы данных
+      database: process.env.DB_DATABASE, // Имя базы данных
       entities: [__dirname + '/**/*.entity{.ts,.js}', User], // Путь до сущностей
       synchronize: true, // Если true, то TypeORM будет синхронизировать схему базы данных с кодом. ОСТОРОЖНО на продакшене!
     }),
